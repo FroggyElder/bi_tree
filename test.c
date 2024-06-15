f0e48068f1ad82919fee63d9efa08976df089674
FroggyElder <shadowskinC@gmail.com>
2024-06-15 13:59:30 +0800

    modified:   test.c

--- test.c
+++ test.c
@@ -21,26 +21,27 @@ void printData (BT_ELEM_TYPE* data) {
 int main () {
     //Create my tree, and fill it with some nonsense data
     BT_ELEM_TYPE tmp_data;
+    int (* cmp)(BT_ELEM_TYPE*,BT_ELEM_TYPE*) = dataNameCmp;
 
     strcpy(tmp_data.name,"Walta"); tmp_data.val = 55;
     struct bi_tree_node* my_tree = BiTreeNodeInit(tmp_data);
     strcpy(tmp_data.name,"Kim"); tmp_data.val = 42;
-    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),dataAgeCmp);
+    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),cmp);
     strcpy(tmp_data.name,"Mike"); tmp_data.val = 67;
-    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),dataAgeCmp);
+    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),cmp);
     strcpy(tmp_data.name,"Jessie"); tmp_data.val = 24;
-    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),dataAgeCmp);
+    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),cmp);
     strcpy(tmp_data.name,"Saul"); tmp_data.val = 45;
-    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),dataAgeCmp);
+    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),cmp);
     strcpy(tmp_data.name,"Gus"); tmp_data.val = 80;
-    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),dataAgeCmp);
+    BiTreeInsertInOrder(my_tree,BiTreeNodeInit(tmp_data),cmp);
 
     BiTreePrintInOrder(my_tree,printData);
 
     printf("\n");
-    BiTreeDeleteInOrder(&my_tree->l_branch);
+    strcpy(tmp_data.name,"Saul"); tmp_data.val = 45;
+    printData(&BiTreeIndexInOrder(my_tree,tmp_data,cmp)->data);
 
-    BiTreePrintInOrder(my_tree,printData);
 
     BiTreeDestroy(my_tree);
 
